e600cbbf4899138da765e6ae65ed90a0c805e291
kimhyeok <kimmang940715@naver.com>
2022-12-10 03:12:57 +0900

    add Manager & Folder

---
+++ Unity_Basic/Basic/Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = GameObject.Find("@Manager");
+        if (go == null)
+        {
+            go = new GameObject();
+            go.GetComponentInChildren<Manager>();
+
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
